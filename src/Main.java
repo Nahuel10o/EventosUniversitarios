ab4fb7d524158932cd1c283e97a769e86afc2119
Nahuel Emiliano Ontiveros Aguilar <109165755+Nahuel10o@users.noreply.github.com>
2026-08-13 10:49:45 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        EventoUniversitarios Fiesta = new EventoUniversitarios("3452", "Graduación", 100, true, 3);
+        EventoUniversitarios Fiesta = new EventoUniversitarios("3452", "Graduación", 100, true, 234, "Meet And Greed", 50, 20);
         Fiesta.mostrar();
     }
 }
