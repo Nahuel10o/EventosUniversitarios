af061f902553b451f6a0a607a1b14578a2d43abb
Nahuel Ontiveros <nahuelnahuel207@gmail.com>
2026-08-18 13:18:20 -0300

    Avance 2

--- src/EventoUniversitarios.java
+++ src/EventoUniversitarios.java
@@ -3,14 +3,25 @@ public class EventoUniversitarios {
     private String titulo;
     private double costoBase;
     private boolean gratuito;
-    private int cantEventos;
+    private static int cantEventos;
 
-    public EventoUniversitarios (String Id, String titulo, double costoBase, boolean gratuito, int cantEventos){
+    private Sala sala;
+    private Actividad actividad;
+
+    public EventoUniversitarios (String Id, String titulo, double costoBase, boolean gratuito, int id, String nombre, int cupoMaximo, int CUPO_MINIMO){
         this.Id = Id;
         this.titulo = titulo;
         this.costoBase = costoBase;
         this.gratuito = gratuito;
-        this.cantEventos = cantEventos;
+        this.actividad = new Actividad(id,titulo, cupoMaximo, CUPO_MINIMO);
+        cantEventos++;
+    }
+
+    public EventoUniversitarios (EventoUniversitarios otro) {
+        this.Id = otro.Id;
+        this.titulo = otro.titulo;
+        this.costoBase = otro.costoBase;
+        this.gratuito = otro.gratuito;
     }
 
     public String getTitulo() {
@@ -37,18 +48,33 @@ public class EventoUniversitarios {
         this.gratuito = gratuito;
     }
 
-    public int getCantEventos() {
+    public static int getCantEventos() {
         return cantEventos;
     }
 
-    public void setCantEventos(int cantEventos) {
-        this.cantEventos = cantEventos;
+    public void agregarSala(Sala sala){
+        this.sala = sala;
     }
+
+    //metodos
     public void mostrar(){
         System.out.println("Id: " + Id);
         System.out.println("titulo: " + titulo );
-        System.out.println("csotoBase: " + costoBase);
+        System.out.println("costoBase: " + costoBase);
         System.out.println("gratuito: " + gratuito );
         System.out.println("cantEventos: " + cantEventos );
     }
+    public double calcularCostoEstimado(){
+        if (this.gratuito)
+            return 0;
+        else
+            return costoBase;
+    }
+    public void asignarSala(){
+
+    }
+    public void crearActividad(int id, String titulo, int cupo){
+
+    }
+
 }
